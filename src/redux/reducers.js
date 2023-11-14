0a42bd4ea40e90e74599de98f01dd267ef85c39c
aadesh <aadeshpandiri@gmail.com>
2023-11-14 16:00:20 +0530

    changes 2.0

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -8,7 +8,7 @@ const initialState = {
     },
     {
       imgUrl: 'https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aWZlbCUyMHRvd2VyfGVufDB8fDB8fHww',
-      imgName: 'Effiel Tower',
+      imgName: 'Eiffel Tower',
       imgLocation: 'Paris',
       imgRating: '4',
     },
